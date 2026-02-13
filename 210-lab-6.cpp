87cf5ccead7da0c478da302b4613a900a2f6b945
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-12 17:29:55 -0800

    4th Commit: enterArrayData function

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -10,12 +10,19 @@ double sumArray(double*);      // dynamic arrays via pointers.
 
 int main()
 {
+    double *array = nullptr;
+    array = new double[SIZE];
+    enterArrayData(array);
     cout << "Hello world! " << endl;
     return 0;
 }
 
-void enterArrayData(double*)
+void enterArrayData(double *arr)
 {
-    
+    for (int i = 0; i < SIZE; i++)
+    {   
+        cout << "Element #" << i << ": ";
+        cin >> *(arr + i);
+    }
 
 }
