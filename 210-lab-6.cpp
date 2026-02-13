24c4fb88bfc4d84768775f56f3dc42669686e12b
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-12 17:05:40 -0800

    2nd Commit: Prototype Functions

---
+++ 210-lab-6.cpp
@@ -0,0 +1,15 @@
+//COMSC-210|Lab 6|Noel Mier-Luna
+#include <iostream>
+
+using namespace std;
+const int SIZE = 5;
+
+void enterArrayData();
+void outputArrayData();
+double sumArray();
+
+int main()
+{
+    cout << "Hello world! " << endl;
+    return 0;
+}
