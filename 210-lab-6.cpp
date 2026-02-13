3f52767a92aa401b42a3f62d09f0a6eb514bbc9d
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-12 17:16:17 -0800

    3rd Commit

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,15 +1,21 @@
-//COMSC-210|Lab 6|Noel Mier-Luna
+// COMSC-210|Lab 6|Noel Mier-Luna
 #include <iostream>
 
 using namespace std;
 const int SIZE = 5;
 
-void enterArrayData();
-void outputArrayData();
-double sumArray();
+void enterArrayData(double*);
+void outputArrayData(double*); // Pointer as parameters to pass
+double sumArray(double*);      // dynamic arrays via pointers.
 
 int main()
 {
     cout << "Hello world! " << endl;
     return 0;
+}
+
+void enterArrayData(double*)
+{
+    
+
 }
